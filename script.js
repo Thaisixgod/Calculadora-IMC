86aa53ec423a70278b7cdeaaa2e8ec6b808fb124
Thaisigod <t.vicente35.tv@gmail.com>
2022-09-29 22:50:59 -0300

    script JS modified

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-const name = document.getElementById("name");
-const altura =  document.getElementById("altura");
-const peso = document.getElementById("peso");
 
+const calcular = document.getElementById("calcular");
+
+
+function imc () {
+    const nome = document.getElementById("nome").value;
+    const altura =  document.getElementById("altura").value;
+    const peso = document.getElementById("peso").value;
+    const resultado =document.getElementById("resultado");
+
+    if (nome !== "" &&  altura !== "" && peso !== ""){
+ 
+        const ValorIMC = (peso / (altura * altura)).toFixed(2);
+        
+        resultado.textContent = ValorIMC;
+ 
+    } else {
+ 
+    resultado.textContent = "Preencha todos os campos";
+    }
+
+}
+
+calcular.addEventListener("click", imc);
